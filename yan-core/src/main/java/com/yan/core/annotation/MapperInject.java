fcf0ecc0f0f1aea915e2695b43a452c3c77d62b7
micyo202 <micyo202@163.com>
2017-12-02 15:40:00 +0800

    更新框架核心模块

---
+++ yan-core/src/main/java/com/yan/core/annotation/MapperInject.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 Yanzheng [https://github.com/micyo202/yan-demo]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.yan.core.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 名称：MapperInject<br>
+ *
+ * 描述：自定义注解（注入Mapper对象）<br>
+ *
+ * @author Yanzheng 严正<br>
+ * 时间：<br>
+ * 2017-08-24 17:39:37<br>
+ * 版权：<br>
+ * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface MapperInject {
+	
+	/**
+	 * 对象类型（默认 Object 则认为是 DelegateMapper 类型）<br>
+	 *
+	 * @return Class<?> 对象类型（反射机制）
+	 */
+	Class<?> value() default Object.class;
+}
