6360537e4fe2af90a8b7f6e07388a4d25c6d3327
micyo202 <micyo202@163.com>
2018-01-06 22:46:04 +0800

    ActiveMQ消息消费者监听

---
+++ yan-core/src/main/java/com/yan/core/listener/TopicMessageListener.java
@@ -0,0 +1,32 @@
+package com.yan.core.listener;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * 名称：TopicMessageListener<br>
+ * <p>
+ * 描述：Topic消息消费者监听器<br>
+ *
+ * @author Yanzheng 严正<br>
+ * 时间：<br>
+ * 2018-01-06 22:34:03<br>
+ * 版权：<br>
+ * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
+ */
+public class TopicMessageListener implements MessageListener {
+
+    @Override
+    public void onMessage(Message message) {
+        TextMessage textMsg = (TextMessage) message;
+        try {
+            System.out.println("Topic消息接收者，收到消息内容：" + textMsg.getText());
+            System.out.println("Topic消息接收成功！开始调用并执行service方法...");
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
