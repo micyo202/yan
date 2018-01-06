d3b7e0c7bec92c24c9ecf85fc580a767f2ee04ab
micyo202 <micyo202@163.com>
2018-01-06 22:47:03 +0800

    ActiveMQ消息传输服务

---
+++ yan-web/src/main/java/com/yan/web/controller/jms/JmsController.java
@@ -0,0 +1,64 @@
+package com.yan.web.controller.jms;
+
+import com.yan.api.jms.MessageSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.jms.Destination;
+
+/**
+ * 名称：JmsController<br>
+ * <p>
+ * 描述：jms消息传输<br>
+ *
+ * @author Yanzheng 严正<br>
+ * 时间：<br>
+ * 2018-01-06 22:28:15<br>
+ * 版权：<br>
+ * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
+ */
+@Controller
+@RequestMapping("/jms")
+public class JmsController {
+
+    /**
+     * 一对一队列消息
+     */
+    @Autowired
+    private MessageSenderService queueSenderService;
+    @Autowired
+    private Destination queueDestination;
+
+    /**
+     * pub/sub模型（发布/订阅）
+     */
+    @Autowired
+    private MessageSenderService topicSenderService;
+    @Autowired
+    private Destination topicDestination;
+
+    /**
+     * 队列消息测试方法
+     */
+    @RequestMapping("/queue/test")
+    @ResponseBody
+    public String queueTest() {
+        System.out.println("Queue消息队列发送消息");
+        queueSenderService.sendQueue(queueDestination, "这是发送的Queue消息");
+        return "Queue Send Success";
+    }
+
+    /**
+     * 订阅消息测试
+     */
+    @RequestMapping("/topic/test")
+    @ResponseBody
+    public String topicTest() {
+        System.out.println("Topic消息队列发送消息");
+        topicSenderService.sendTopic(topicDestination, "这是发送的Topic消息");
+        return "Topic Send Success";
+    }
+
+}
