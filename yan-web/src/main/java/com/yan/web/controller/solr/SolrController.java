e966743c4b8c26e91f05b98b0369eb775185cae6
micyo202 <micyo202@163.com>
2018-01-06 22:50:08 +0800

    添加搜索引擎模块

--- yan-web/src/main/java/com/yan/web/controller/solr/SolrController.java
+++ yan-web/src/main/java/com/yan/web/controller/solr/SolrController.java
@@ -1,10 +1,8 @@
 package com.yan.web.controller.solr;
 
-import com.yan.core.controller.BaseController;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,9 +13,21 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+
+/**
+ * 名称：SolrController<br>
+ * <p>
+ * 描述：Solr所搜引擎服务<br>
+ *
+ * @author Yanzheng 严正<br>
+ * 时间：<br>
+ * 2018-01-06 22:32:59<br>
+ * 版权：<br>
+ * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
+ */
 @Controller
 @RequestMapping("/solr")
-public class SolrController extends BaseController {
+public class SolrController {
 
     @Autowired
     private HttpSolrClient httpSolrClient;
@@ -32,7 +42,7 @@ public class SolrController extends BaseController {
             QueryResponse queryResponse = httpSolrClient.query(query);
             SolrDocumentList docs = queryResponse.getResults();
             Iterator it = docs.iterator();
-            while(it.hasNext()){
+            while (it.hasNext()) {
                 list.add(it.next());
             }
         } catch (Exception e) {
