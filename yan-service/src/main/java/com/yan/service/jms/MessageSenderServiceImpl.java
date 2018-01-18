a17001885f7fb13434a12bd39ca8f995dfa80a52
micyo202 <micyo202@163.com>
2018-01-18 16:22:45 +0800

    添加jms消息发送接口实现类

--- yan-service/src/main/java/com/yan/service/jms/MessageSenderServiceImpl.java
+++ yan-service/src/main/java/com/yan/service/jms/MessageSenderServiceImpl.java
@@ -21,7 +21,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.yan.core.jms;
+package com.yan.service.jms;
 
 import com.yan.api.jms.MessageSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
