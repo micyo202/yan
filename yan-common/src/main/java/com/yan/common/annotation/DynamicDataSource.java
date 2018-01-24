e04ee19ad04763f2ce6b0b2f86537242807c7f36
micyo202 <micyo202@163.com>
2018-01-24 17:17:22 +0800

    更新动态数据源注解类

--- yan-common/src/main/java/com/yan/common/annotation/DynamicDataSource.java
+++ yan-common/src/main/java/com/yan/common/annotation/DynamicDataSource.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2017 Yanzheng [https://github.com/micyo202/yan]
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -8,10 +8,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -21,52 +21,32 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.yan.service.jms;
+package com.yan.common.annotation;
 
-import com.yan.api.jms.MessageSenderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.stereotype.Service;
+import com.yan.common.constant.DataSourceName;
 
-import javax.jms.Destination;
+import java.lang.annotation.*;
 
 /**
- * 名称：MessageSenderServiceImpl<br>
+ * 名称：DynamicDataSource<br>
  * <p>
- * 描述：Jms消息（生产者）发送服务接口实现类<br>
+ * 描述：动态数据源切换注解（默认切换扩展数据源）<br>
  *
  * @author Yanzheng 严正<br>
  * 时间：<br>
- * 2018-01-06 22:34:03<br>
+ * 2017-09-06 11:17:12<br>
  * 版权：<br>
  * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
  */
-@Service
-public class MessageSenderServiceImpl implements MessageSenderService {
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface DynamicDataSource {
 
-    @Autowired
-    private JmsTemplate jmsQueueTemplate;
-
-    @Autowired
-    private JmsTemplate jmsTopicTemplate;
-
-    @Override
-    public void sendQueue(Destination destination, String message) {
-        jmsQueueTemplate.send(destination, session -> session.createTextMessage(message));
-    }
-
-    @Override
-    public void sendTopic(Destination destination, String message) {
-        jmsTopicTemplate.send(destination, session -> session.createTextMessage(message));
-    }
-
-    @Override
-    public void sendQueue(String name, String message) {
-        jmsQueueTemplate.send(name, session -> session.createTextMessage(message));
-    }
-
-    @Override
-    public void sendTopic(String name, String message) {
-        jmsTopicTemplate.send(name, session -> session.createTextMessage(message));
-    }
+    /**
+     * 需要切换的数据源名称<br>
+     *
+     * @return DataSourceName
+     */
+    public DataSourceName value() default DataSourceName.EXTEND;
 }
