9483c1c038bb5b27f93e1edadbdd151568deefd7
micyo202 <micyo202@163.com>
2018-01-24 17:13:36 +0800

    添加测试接口

---
+++ yan-api/src/main/java/com/yan/api/demo/DemoService.java
@@ -0,0 +1,18 @@
+package com.yan.api.demo;
+
+/**
+ * 名称：DemoService<br>
+ * <p>
+ * 描述：测试接口服务<br>
+ *
+ * @author Yanzheng 严正<br>
+ * 时间：<br>
+ * 2018-01-18 09:39:50<br>
+ * 版权：<br>
+ * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
+ */
+public interface DemoService {
+
+    String sayHello(String name);
+
+}
