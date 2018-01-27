3f173fbd5350ffcd24af65b97f635c2843965a8e
micyo202 <micyo202@163.com>
2018-01-28 01:09:37 +0800

    更新基本的Servce接口及实现类

--- yan-common/src/main/java/com/yan/common/service/BaseService.java
+++ yan-common/src/main/java/com/yan/common/service/BaseService.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * 描述：TODO<br>
+ * 描述：通用base接口，封装CRUD操作<br>
  *
  * @author Yanzheng 严正<br>
  * 时间：<br>
@@ -14,27 +14,183 @@ import java.util.List;
  * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
  */
 public interface BaseService<Record, Example> {
+    /**
+     * 根据条件查询记录数量
+     *
+     * @param example
+     * @return
+     */
+    int countByExample(Example example);
 
-    long countByExample(Example example);
-
+    /**
+     * 根据条件删除记录
+     *
+     * @param example
+     * @return
+     */
     int deleteByExample(Example example);
 
-    int deleteByPrimaryKey(String key);
+    /**
+     * 根据主键删除记录
+     *
+     * @param id
+     * @return
+     */
+    int deleteByPrimaryKey(String id);
 
+    /**
+     * 插入记录
+     *
+     * @param record
+     * @return
+     */
     int insert(Record record);
 
+    /**
+     * 插入记录有效字段
+     *
+     * @param record
+     * @return
+     */
     int insertSelective(Record record);
 
+    /**
+     * 根据条件查询记录，附带BLOB字段
+     *
+     * @param example
+     * @return
+     */
+    List<Record> selectByExampleWithBLOBs(Example example);
+
+    /**
+     * 根据条件查询记录
+     *
+     * @param example
+     * @return
+     */
     List<Record> selectByExample(Example example);
 
-    Record selectByPrimaryKey(String key);
+    /**
+     * 根据条件查询记录并按页码分页，附带BLOB字段
+     *
+     * @param example  条件
+     * @param pageNum  页数
+     * @param pageSize 每页记录数
+     * @return
+     */
+    List<Record> selectByExampleWithBLOBsForStartPage(Example example, Integer pageNum, Integer pageSize);
 
+    /**
+     * 根据条件查询记录并按页码分页
+     *
+     * @param example  条件
+     * @param pageNum  页数
+     * @param pageSize 每页记录数
+     * @return
+     */
+    List<Record> selectByExampleForStartPage(Example example, Integer pageNum, Integer pageSize);
+
+    /**
+     * 根据条件查询记录并按最后记录数分页，附带BLOB字段
+     *
+     * @param example 条件
+     * @param offset  跳过数量
+     * @param limit   查询数量
+     * @return
+     */
+    List<Record> selectByExampleWithBLOBsForOffsetPage(Example example, Integer offset, Integer limit);
+
+    /**
+     * 根据条件查询记录并按最后记录数分页
+     *
+     * @param example 条件
+     * @param offset  跳过数量
+     * @param limit   查询数量
+     * @return
+     */
+    List<Record> selectByExampleForOffsetPage(Example example, Integer offset, Integer limit);
+
+    /**
+     * 根据条件查询第一条记录
+     *
+     * @param example
+     * @return
+     */
+    Record selectFirstByExample(Example example);
+
+    /**
+     * 根据条件查询第一条记录，附带BLOB字段
+     *
+     * @param example
+     * @return
+     */
+    Record selectFirstByExampleWithBLOBs(Example example);
+
+    /**
+     * 根据主键查询记录
+     *
+     * @param id
+     * @return
+     */
+    Record selectByPrimaryKey(String id);
+
+    /**
+     * 根据条件更新有效字段
+     *
+     * @param record
+     * @param example
+     * @return
+     */
     int updateByExampleSelective(@Param("record") Record record, @Param("example") Example example);
 
+    /**
+     * 根据条件更新记录有效字段，附带BLOB字段
+     *
+     * @param record
+     * @param example
+     * @return
+     */
+    int updateByExampleWithBLOBs(@Param("record") Record record, @Param("example") Example example);
+
+    /**
+     * 根据条件更新记录
+     *
+     * @param record
+     * @param example
+     * @return
+     */
     int updateByExample(@Param("record") Record record, @Param("example") Example example);
 
+    /**
+     * 根据主键更新记录有效字段
+     *
+     * @param record
+     * @return
+     */
     int updateByPrimaryKeySelective(Record record);
 
+    /**
+     * 根据主键更新记录，附带BLOB字段
+     *
+     * @param record
+     * @return
+     */
+    int updateByPrimaryKeyWithBLOBs(Record record);
+
+    /**
+     * 根据主键更新记录
+     *
+     * @param record
+     * @return
+     */
     int updateByPrimaryKey(Record record);
+
+    /**
+     * 根据主键批量删除记录
+     *
+     * @param ids
+     * @return
+     */
+    int deleteByPrimaryKeys(String ids);
 
 }
--- yan-common/src/main/java/com/yan/common/service/impl/BaseServiceImpl.java
+++ yan-common/src/main/java/com/yan/common/service/impl/BaseServiceImpl.java
@@ -1,6 +1,9 @@
 package com.yan.common.service.impl;
 
+import com.github.pagehelper.PageHelper;
 import com.yan.common.service.BaseService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.lang.reflect.InvocationTargetException;
@@ -8,7 +11,7 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 /**
- * 描述：TODO<br>
+ * 描述：基本base接口，CRUD泛型接口实现类，抽象类只能被继承<br>
  *
  * @author Yanzheng 严正<br>
  * 时间：<br>
@@ -22,68 +25,363 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
     private Mapper mapper;
 
     @Override
-    public long countByExample(Example example) {
+    public int countByExample(Example example) {
+        try {
+            Method countByExample = mapper.getClass().getDeclaredMethod("countByExample", example.getClass());
+            Object result = countByExample.invoke(mapper, example);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
     @Override
     public int deleteByExample(Example example) {
+        try {
+            Method deleteByExample = mapper.getClass().getDeclaredMethod("deleteByExample", example.getClass());
+            Object result = deleteByExample.invoke(mapper, example);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
     @Override
-    public int deleteByPrimaryKey(String key) {
+    public int deleteByPrimaryKey(String id) {
+        try {
+            Method deleteByPrimaryKey = mapper.getClass().getDeclaredMethod("deleteByPrimaryKey", id.getClass());
+            Object result = deleteByPrimaryKey.invoke(mapper, id);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
     @Override
     public int insert(Record record) {
+        try {
+            Method insert = mapper.getClass().getDeclaredMethod("insert", record.getClass());
+            Object result = insert.invoke(mapper, record);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
     @Override
     public int insertSelective(Record record) {
+        try {
+            Method insertSelective = mapper.getClass().getDeclaredMethod("insertSelective", record.getClass());
+            Object result = insertSelective.invoke(mapper, record);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
     @Override
-    public List<Record> selectByExample(Example example) {
+    public List<Record> selectByExampleWithBLOBs(Example example) {
         try {
-            Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example.getClass());
-            Object result = selectByExample.invoke(mapper, example);
+            Method selectByExampleWithBLOBs = mapper.getClass().getDeclaredMethod("selectByExampleWithBLOBs", example.getClass());
+            Object result = selectByExampleWithBLOBs.invoke(mapper, example);
             return (List<Record>) result;
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
             e.printStackTrace();
         }
         return null;
     }
 
     @Override
-    public Record selectByPrimaryKey(String key) {
+    public List<Record> selectByExample(Example example) {
+        try {
+            Method selectByExample;
+            if (null != example) {
+                selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example.getClass());
+            } else {
+                selectByExample = mapper.getClass().getDeclaredMethod("selectByExample");
+            }
+            Object result = selectByExample.invoke(mapper, example);
+            return (List<Record>) result;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
-    public int updateByExampleSelective(Record record, Example example) {
+    public List<Record> selectByExampleWithBLOBsForStartPage(Example example, Integer pageNum, Integer pageSize) {
+        try {
+            Method selectByExampleWithBLOBs = mapper.getClass().getDeclaredMethod("selectByExampleWithBLOBs", example.getClass());
+            PageHelper.startPage(pageNum, pageSize, false);
+            Object result = selectByExampleWithBLOBs.invoke(mapper, example);
+            return (List<Record>) result;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Record> selectByExampleForStartPage(Example example, Integer pageNum, Integer pageSize) {
+        try {
+            Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example.getClass());
+            PageHelper.startPage(pageNum, pageSize, false);
+            Object result = selectByExample.invoke(mapper, example);
+            return (List<Record>) result;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Record> selectByExampleWithBLOBsForOffsetPage(Example example, Integer offset, Integer limit) {
+        try {
+            Method selectByExampleWithBLOBs = mapper.getClass().getDeclaredMethod("selectByExampleWithBLOBs", example.getClass());
+            PageHelper.offsetPage(offset, limit, false);
+            Object result = selectByExampleWithBLOBs.invoke(mapper, example);
+            return (List<Record>) result;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Record> selectByExampleForOffsetPage(Example example, Integer offset, Integer limit) {
+        try {
+            Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example.getClass());
+            PageHelper.offsetPage(offset, limit, false);
+            Object result = selectByExample.invoke(mapper, example);
+            return (List<Record>) result;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Record selectFirstByExample(Example example) {
+        try {
+            Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example.getClass());
+            List<Record> result = (List<Record>) selectByExample.invoke(mapper, example);
+            if (null != result && result.size() > 0) {
+                return result.get(0);
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Record selectFirstByExampleWithBLOBs(Example example) {
+        try {
+            Method selectByExampleWithBLOBs = mapper.getClass().getDeclaredMethod("selectByExampleWithBLOBs", example.getClass());
+            List<Record> result = (List<Record>) selectByExampleWithBLOBs.invoke(mapper, example);
+            if (null != result && result.size() > 0) {
+                return result.get(0);
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Record selectByPrimaryKey(String id) {
+        try {
+            Method selectByPrimaryKey = mapper.getClass().getDeclaredMethod("selectByPrimaryKey", id.getClass());
+            Object result = selectByPrimaryKey.invoke(mapper, id);
+            return (Record) result;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public int updateByExampleSelective(@Param("record") Record record, @Param("example") Example example) {
+        try {
+            Method updateByExampleSelective = mapper.getClass().getDeclaredMethod("updateByExampleSelective", record.getClass(), example.getClass());
+            Object result = updateByExampleSelective.invoke(mapper, record, example);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
     @Override
-    public int updateByExample(Record record, Example example) {
+    public int updateByExampleWithBLOBs(@Param("record") Record record, @Param("example") Example example) {
+        try {
+            Method updateByExampleWithBLOBs = mapper.getClass().getDeclaredMethod("updateByExampleWithBLOBs", record.getClass(), example.getClass());
+            Object result = updateByExampleWithBLOBs.invoke(mapper, record, example);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Override
+    public int updateByExample(@Param("record") Record record, @Param("example") Example example) {
+        try {
+            Method updateByExample = mapper.getClass().getDeclaredMethod("updateByExample", record.getClass(), example.getClass());
+            Object result = updateByExample.invoke(mapper, record, example);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
     @Override
     public int updateByPrimaryKeySelective(Record record) {
+        try {
+            Method updateByPrimaryKeySelective = mapper.getClass().getDeclaredMethod("updateByPrimaryKeySelective", record.getClass());
+            Object result = updateByPrimaryKeySelective.invoke(mapper, record);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Override
+    public int updateByPrimaryKeyWithBLOBs(Record record) {
+        try {
+            Method updateByPrimaryKeyWithBLOBs = mapper.getClass().getDeclaredMethod("updateByPrimaryKeyWithBLOBs", record.getClass());
+            Object result = updateByPrimaryKeyWithBLOBs.invoke(mapper, record);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 
     @Override
     public int updateByPrimaryKey(Record record) {
+        try {
+            Method updateByPrimaryKey = mapper.getClass().getDeclaredMethod("updateByPrimaryKey", record.getClass());
+            Object result = updateByPrimaryKey.invoke(mapper, record);
+            return Integer.parseInt(String.valueOf(result));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Override
+    public int deleteByPrimaryKeys(String ids) {
+        try {
+            if (StringUtils.isBlank(ids)) {
+                return 0;
+            }
+            String[] idArray = ids.split("-");
+            int count = 0;
+            for (String idStr : idArray) {
+                if (StringUtils.isBlank(idStr)) {
+                    continue;
+                }
+                Integer id = Integer.parseInt(idStr);
+                Method deleteByPrimaryKey = mapper.getClass().getDeclaredMethod("deleteByPrimaryKey", id.getClass());
+                Object result = deleteByPrimaryKey.invoke(mapper, id);
+                count += Integer.parseInt(String.valueOf(result));
+            }
+            return count;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return 0;
     }
 }
