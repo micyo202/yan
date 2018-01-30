2f50dabd16b779196598165e1f6c32821514a0e3
micyo202 <micyo202@163.com>
2018-01-30 17:32:41 +0800

    更新橘色菜单关系接口

---
+++ yan-api/src/main/java/com/yan/api/role/RoleMenuRelService.java
@@ -0,0 +1,17 @@
+package com.yan.api.role;
+
+import com.yan.common.service.BaseService;
+import com.yan.dao.model.role.RoleMenuRel;
+import com.yan.dao.model.role.RoleMenuRelExample;
+
+/**
+ * 描述：TODO<br>
+ *
+ * @author Yanzheng 严正<br>
+ * 时间：<br>
+ * 2018/1/27 下午11:38<br>
+ * 版权：<br>
+ * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
+ */
+public interface RoleMenuRelService extends BaseService<RoleMenuRel, RoleMenuRelExample> {
+}
