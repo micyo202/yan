c7282d0a9259677ca042945b79c93ee148479c61
micyo202 <micyo202@163.com>
2018-01-30 17:30:52 +0800

    更新自定义代理Service接口服务

--- yan-api/src/main/java/com/yan/api/persistence/DelegateService.java
+++ yan-api/src/main/java/com/yan/api/persistence/DelegateService.java
@@ -1,26 +1,3 @@
-/*
- * Copyright (c) 2017 Yanzheng [https://github.com/micyo202/yan]
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 package com.yan.api.persistence;
 
 import com.yan.common.model.PageModel;
@@ -29,9 +6,9 @@ import org.apache.ibatis.session.RowBounds;
 import java.util.List;
 
 /**
- * 名称：DelegateMapper<br>
+ * 名称：DelegateService<br>
  * <p>
- * 描述：自定义的代理Mapper接口，封装了常用CRUD操作<br>
+ * 描述：自定义的代理Service接口，封装了常用自定义sql的CRUD操作<br>
  *
  * @author Yanzheng 严正<br>
  * 时间：<br>
@@ -39,7 +16,7 @@ import java.util.List;
  * 版权：<br>
  * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
  */
-public interface DelegateMapper {
+public interface DelegateService {
 
     /**
      * 无参查询单个对象<br>
