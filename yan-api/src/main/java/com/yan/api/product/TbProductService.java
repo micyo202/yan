cc5535f615ff627b46684691e3977013e5e100c7
micyo202 <micyo202@163.com>
2018-01-30 17:31:26 +0800

    更新产品模块接口

---
+++ yan-api/src/main/java/com/yan/api/product/TbProductService.java
@@ -0,0 +1,17 @@
+package com.yan.api.product;
+
+import com.yan.common.service.BaseService;
+import com.yan.dao.model.product.TbProduct;
+import com.yan.dao.model.product.TbProductExample;
+
+/**
+ * 描述：TODO<br>
+ *
+ * @author Yanzheng 严正<br>
+ * 时间：<br>
+ * 2018/1/27 下午11:37<br>
+ * 版权：<br>
+ * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
+ */
+public interface TbProductService extends BaseService<TbProduct, TbProductExample> {
+}
