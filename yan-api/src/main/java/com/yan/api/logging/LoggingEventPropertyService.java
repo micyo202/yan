76ba4815aa2a1551e7f4011f2c49e2f9ee65dfd7
micyo202 <micyo202@163.com>
2018-01-30 17:30:04 +0800

    添加日志记录类服务接口

---
+++ yan-api/src/main/java/com/yan/api/logging/LoggingEventPropertyService.java
@@ -0,0 +1,17 @@
+package com.yan.api.logging;
+
+import com.yan.common.service.BaseService;
+import com.yan.dao.model.logging.LoggingEventProperty;
+import com.yan.dao.model.logging.LoggingEventPropertyExample;
+
+/**
+ * 描述：TODO<br>
+ *
+ * @author Yanzheng 严正<br>
+ * 时间：<br>
+ * 2018/1/27 下午11:33<br>
+ * 版权：<br>
+ * Copyright 2017 <a href="https://github.com/micyo202" target="_blank">https://github.com/micyo202</a>. All rights reserved.
+ */
+public interface LoggingEventPropertyService extends BaseService<LoggingEventProperty, LoggingEventPropertyExample> {
+}
